82d385e17f1faa0f55abdab1d6928322b39bea6b
Pedro Aguilar <paguilar@paguilar.org>
2025-02-26 00:21:05 +0100

    scripts/install.sh: Add support for different Buildroot versions
    When patching Buildroot, detect its version by reading BR2_VERSION from
    the main Makefile and apply the corresponding patch set found in the
    in directory with the same name as the Buildroot version inside patches/
    
    Eg. If the detected Buildroot version is 2024.11, apply the patches
    found in the directory patches/2024.11/
    
    Signed-off-by: Pedro Aguilar <paguilar@paguilar.org>

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,20 +22,49 @@ Installs Buildroot's parallel builder
 "
 }
 
+# Get Buildroot version from the variable BR2_VERSION
+# in the main Makefile of the given path
+get_br2_ver()
+{
+    br2_ver_str=$(cat $BR_PATH/Makefile | grep -e "^export BR2_VERSION :=" | awk '{print $4}')
+    major_ver=$(echo $br2_ver_str | awk -F'.' '{print $1}')
+    git_ver=$(echo $br2_ver_str | grep "git")
+
+    # Get the version also when Buildroot is downloaded from a git repo
+    if [ -z $git_ver ]; then
+        minor_ver=$(echo $br2_ver_str | awk -F'.' '{print $2}')
+    else
+        minor_ver=$(echo $br2_ver_str | awk -F'.' '{print $2}' | awk -F'-' '{print $1}')
+    fi
+
+    br2_ver=$major_ver"."$minor_ver
+}
+
 install()
 {
-    echo "creating symlinks..."
+    get_br2_ver
+    echo "Detected Buildroot version (yyyy.mm): $br2_ver"
+    if [ ! -d $PBUILDER_PATH/patches/$br2_ver ]; then
+        echo "Failed to found patches for this Buildroot version: $br2_ver"
+        exit 1
+    fi
+
+    echo "Creating symlinks..."
     cd $BR_PATH/utils
     ln -s $PBUILDER_PATH pbuilder
+    cd - 1>/dev/null
 
     cd $BR_PATH/support/scripts
     ln -s $PBUILDER_PATH/scripts/pbuilder.py .
+    cd - 1>/dev/null
 
-    echo "patching Buildroot Makefile..."
     cd $BR_PATH
-    for i in `ls $PBUILDER_PATH/patches/*.patch`; do
+    echo "Applying patches..."
+    for i in `ls $PBUILDER_PATH/patches/$br2_ver/*.patch`; do
+        #echo "patch -p1 < $i"
         patch -p1 < $i
     done
+    cd - 1>/dev/null
 }
 
 remove()
